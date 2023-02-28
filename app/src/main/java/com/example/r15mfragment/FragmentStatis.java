ab1d8b59827184773744e53b0ce378cf39898db2
GisfaRyangga <gisfa.putra2603@mail.ugm.ac.id>
2023-03-01 00:17:02 +0700

    add fragment statis

---
+++ app/src/main/java/com/example/r15mfragment/FragmentStatis.java
@@ -0,0 +1,21 @@
+package com.example.r15mfragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentStatis extends Fragment {
+
+    public FragmentStatis() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_statis, container, false);
+        return rootView;
+    }
+}
